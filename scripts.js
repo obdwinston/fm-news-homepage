ec3503a74c84c1cf67d0ac72e4f4c1f358e92a87
Winston Ong <obdwinston@gmail.com>
2023-12-28 01:23:57 +0800

    added animejs

--- scripts.js
+++ scripts.js
@@ -13,3 +13,36 @@ menu.addEventListener("click", function () {
     body.style.background = "rgba(0,0,0,0.0)";
   });
 });
+
+anime({
+  targets: ".header",
+  opacity: [0, 1],
+  duration: 1000,
+  easing: "easeInOutQuad",
+});
+
+anime({
+  targets: ".left-image",
+  opacity: [0, 1],
+  translateY: [-15, 0],
+  duration: 500,
+  easing: "easeInOutQuad",
+});
+
+anime({
+  targets: ".left-title, .left-subtitle, .right",
+  opacity: [0, 1],
+  translateY: [15, 0],
+  duration: 500,
+  delay: 100,
+  easing: "easeInOutQuad",
+});
+
+anime({
+  targets: ".footer",
+  opacity: [0, 1],
+  translateY: [15, 0],
+  duration: 500,
+  delay: 300,
+  easing: "easeInOutQuad",
+});
